cf063333be977f29af4bcb8395f868e13127dc10
Ahmed ayoub <aayoub1984@gmail.com>
2015-02-03 06:36:56 +0200

    no message

--- index.php
+++ index.php
@@ -11,16 +11,41 @@
     <div class="layout debug-color10">
       <div class="col-first debug-color1">1</div>
       <div class="col-second debug-color2">2</div>
-      <div class="col-third debug-color3">3</div>
-      <div class="col-fourth debug-color4">4</div>
     </div>
+  </div>
+  <div class="container debug-color33">
+    <div class="col-third debug-color3">3</div>
+  </div>
+  <div class="container debug-color44">
+    <div class="layout-1 debug-color25">3</div>
+    <div class="layout-2 debug-color22">
+      <div class="layout-items debug-color41">item1</div>
+      <div class="layout-items debug-color42">item2</div>
+      <div class="layout-items debug-color43">item3</div>
+      <div class="layout-items debug-color44">item4</div>
+      <div class="layout-items debug-color45">item5</div>
+      <div class="layout-items debug-color46">item6</div>
+    </div>
+    <div class="layout-3 debug-color36">3</div>
+  </div>
+  <div class="container debug-color37">
+    <div class="ahmed debug-color2">ahmed</div>
+    <div class="ayoub debug-color3">ahmed</div>
   </div>
 
-  <div class="container debug-color32">
-    <div class="layout debug-color24">
-      <div class="col-second-first debug-color1">1</div>
-      <div class="col-second-second debug-color2">2</div>
-    </div>
+  <div class="container debug-color50">
+    <div class="ethar debug-color6">ethar</div>
+    <div class="samar debug-color17">samar</div>
+    <div class="alyaa debug-color22">alyaa</div>
   </div>
+
+  <div class="container debug-color40">
+    <div class="baba debug-color9">baba</div>
+    <div class="mama debug-color20">mama</div>
+    <div class="aly debug-color24">aly</div>
+  </div>
+  
+
+
 </body>
 </html>
